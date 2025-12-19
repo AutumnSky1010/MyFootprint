00db73eebf425589b06eab4bf0ba71bef7cf079b
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2025-12-20 00:51:44 +0900

    :hammer: 一旦Noto Sansで統一

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxtjs/google-fonts'],
+  css: [
+    '~/assets/css/main.css'
+  ],
+  googleFonts: {
+    families: {
+      // 日本語フォント: Noto Sans JP
+      'Noto+Sans+JP': [400, 500, 700],
+    },
+    display: 'swap',
+    preload: true,
+    download: true,
+  }
 })
