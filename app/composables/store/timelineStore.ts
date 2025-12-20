467ce7581835c7fcdf5052716411415c6a17e6ac
AutumnSky1010 <66455966+AutumnSky1010@users.noreply.github.com>
2025-12-20 09:13:07 +0900

    :bug: タイムゾーンを指定

--- app/composables/store/timelineStore.ts
+++ app/composables/store/timelineStore.ts
@@ -4,14 +4,14 @@ import type { Timeline } from "../models/timeline";
 export const useTimelineStore = defineStore("TimelineStore", {
   // TODO: 現在は決め打ちだが将来的にはサーバからデータを取得する
   state: (): Timeline => ({
-    date: new Date("2024-01-01"),
+    date: new Date("2024-01-01T00:00:00+09:00"),
     entries: [
       {
         title: "自宅",
         description: "",
         duration: {
-          start: new Date("2024-01-01T10:00:00"),
-          end: new Date("2024-01-01T12:00:00")
+          start: new Date("2024-01-01T10:00:00+09:00"),
+          end: new Date("2024-01-01T12:00:00+09:00")
         },
         type: 'place',
         relatedInformation: {
@@ -23,8 +23,8 @@ export const useTimelineStore = defineStore("TimelineStore", {
         title: "研究室へ移動",
         description: "",
         duration: {
-          start: new Date("2024-01-01T13:00:00"),
-          end: new Date("2024-01-01T14:00:00")
+          start: new Date("2024-01-01T13:00:00+09:00"),
+          end: new Date("2024-01-01T14:00:00+09:00")
         },
         type: 'move',
         relatedInformation: {
@@ -36,8 +36,8 @@ export const useTimelineStore = defineStore("TimelineStore", {
         title: "東京都市大学 計算機ソフトウェア研究室",
         description: "",
         duration: {
-          start: new Date("2024-01-01T14:00:00"),
-          end: new Date("2024-01-01T23:00:00")
+          start: new Date("2024-01-01T14:00:00+09:00"),
+          end: new Date("2024-01-01T23:00:00+09:00")
         },
         type: 'place',
         relatedInformation: {
@@ -49,8 +49,8 @@ export const useTimelineStore = defineStore("TimelineStore", {
         title: "帰宅",
         description: "",
         duration: {
-          start: new Date("2024-01-01T23:00:00"),
-          end: new Date("2024-01-01T23:50:00")
+          start: new Date("2024-01-01T23:00:00+09:00"),
+          end: new Date("2024-01-01T23:50:00+09:00")
         },
         type: 'move',
         relatedInformation: {
@@ -62,8 +62,8 @@ export const useTimelineStore = defineStore("TimelineStore", {
         title: "自宅",
         description: "",
         duration: {
-          start: new Date("2024-01-01T23:50:00"),
-          end: new Date("2024-01-01T23:59:00")
+          start: new Date("2024-01-01T23:50:00+09:00"),
+          end: new Date("2024-01-01T23:59:00+09:00")
         },
         type: 'place',
         relatedInformation: {
